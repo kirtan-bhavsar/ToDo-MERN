f56e881a8b12738225ec7a7612b1954932380cd7
kirtan-bhavsar <kirtanbhavsar.2491@gmail.com>
2025-04-10 10:34:31 +0530

    added_App.cssApp.jsx_CheckboxAnimation_Dt:10042025

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,62 +1,72 @@
 import React, { useState } from "react";
-import axios from 'axios';
+import axios from "axios";
 // import { useState } from "react";
-import {FaMapMarker} from 'react-icons/fa';
-
+import { FaMapMarker } from "react-icons/fa";
 
 const App = () => {
-
-  const [data,setData] = useState({
-    title:""
+  const [data, setData] = useState({
+    title: "",
   });
 
-const addTask = async(e) => {
+  const addTask = async (e) => {
+    console.log(data);
 
-  console.log(data);
+    e.preventDefault();
 
-  e.preventDefault();
-
-  try {
-    await axios.post('http://localhost:5500/api/v1/add',data);
-    console.log("API Call successful");
-  } catch (error) {
-    console.log(error);
-  }
-
-}
+    try {
+      await axios.post("http://localhost:5500/api/v1/add", data);
+      console.log("API Call successful");
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <>
       <div className="container-fluid bg-dark vh-100  align-items-center d-flex flex-column">
         <h1 className="text-custom-heading-color my-2 fw-bold">My Todos</h1>
         <div className="add-todo d-flex my-5">
-        <input
-          type="text"
-          htmlFor="title"
-          name="title"
-          placeholder="Add a new task..."
-          onChange={(e) => setData({...data,title:e.target.value})}
-          className="bg-custom-secondary-color addtask-input rounded-3 p-2 text-custom-secondary-color"
-        />
-        <button className="" type="button" onClick={addTask}>Add</button>
-      </div>
-      <div className="list-todos">
-        <li className="text-custom-primary-color">
+          <input
+            type="text"
+            htmlFor="title"
+            name="title"
+            placeholder="Add a new task..."
+            onChange={(e) => setData({ ...data, title: e.target.value })}
+            className="bg-custom-secondary-color addtask-input rounded-3 p-2 text-custom-secondary-color"
+          />
+          <button className="" type="button" onClick={addTask}>
+            Add
+          </button>
+        </div>
+        <div className="list-todos">
+          {/* <li className="text-custom-primary-color">
           <div className="todo-task">
             <span><input type="checkbox" /></span>
             task title
             <span>icon 2</span>
             <span>icon 3</span>
           </div>
-        </li>
-        <li className="text-custom-primary-color">
-          <div className="todo-task d-flex">
-            <span style={ {width:"10%"} }><input type="checkbox" /></span>
-            <span style={ {width:"50%"} }>Todo Title</span>
-            <span style={ {width:"15%"} }><FaMapMarker/></span>
-            <span style={ {width:"15%"} }>icon 3</span>
-          </div>
-        </li>
+        </li> */}
+          <li className="text-custom-primary-color">
+            <div className="todo-task d-flex bg-custom-secondary-color h-25">
+              {/* <span style={ {width:"5%"} } className="mx-2"><input type="checkbox" /></span> */}
+              <span style={{ width: "5%" }} className="mx-2 py-3">
+                <input type="checkbox" id="isTodoCompleted" hidden/>
+                <label
+                  htmlFor="isTodoCompleted"
+                  id="isTodoCompletedLabel"
+                >
+                  <span className="line line1"></span>
+                  <span className="line line2"></span>
+                </label>
+              </span>
+              <span style={{ width: "85%" }} className=" py-3">Todo Title</span>
+              <span className="py-2" style={{ width: "5%" }}>
+                <FaMapMarker />
+              </span>
+              <span className="py-2" style={{ width: "5%" }}>ic</span>
+            </div>
+          </li>
         </div>
       </div>
     </>
