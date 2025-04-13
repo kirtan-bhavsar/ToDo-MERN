51a61b358febddacd6739fc3c456eb9d6e7426d5
kirtan-bhavsar <kirtanbhavsar.2491@gmail.com>
2025-04-14 01:49:24 +0530

    added_editTodoTitleWithUIImplementation_Dt:14042025

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,165 +1,210 @@
-  import React, { useEffect, useState,useRef } from "react";
-  import axios from "axios";
-  import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-  // import { faPenToSquare as faPenToSquareRegular} from '@fortawesome/free-solid-svg-icons';
-  import { faPenToSquare as faPenToSquareRegular } from '@fortawesome/free-regular-svg-icons';
-  import { faX } from '@fortawesome/free-solid-svg-icons';
-  import { faTrash } from '@fortawesome/free-solid-svg-icons';
-  import { faPenToSquare } from '@fortawesome/free-solid-svg-icons';
+import React, { useEffect, useState, useRef } from "react";
+import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPenToSquare as faPenToSquareRegular } from "@fortawesome/free-regular-svg-icons";
+import { faX } from "@fortawesome/free-solid-svg-icons";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
+import { faCheck } from "@fortawesome/free-solid-svg-icons";
 
-  const App = () => {
+const App = () => {
+  // Use States
+  const [isEditing, setEditing] = useState(null);
 
-    const [data, setData] = useState({
-      title: "",
-    });
+  const [data, setData] = useState({
+    title: "",
+  });
 
-    const [todos,setTodos] = useState([]);
+  const [editData, setEditData] = useState({
+    title: "",
+  });
 
-    const addInputRef = useRef(null); 
+  const [todos, setTodos] = useState([]);
 
-    const fetchData = async() => {
+  const addInputRef = useRef(null);
 
-      try {
-        const apiData = await axios.get('http://localhost:5500/api/v1/todos');
-        setTodos(apiData.data);
-        console.log("fetchData api called successfully");
-      } catch (error) {
-        console.log(error);
-      }
+  // const editInputRef = useRef(null);
 
+  // Event Listeners for tasks
+  const fetchData = async () => {
+    try {
+      const apiData = await axios.get("/api/v1/todos");
+      setTodos(apiData.data);
+      console.log("fetchData api called successfully");
+    } catch (error) {
+      console.log(error);
     }
-
-    useEffect(()=> {
-      
-      fetchData();
-
-    },[]);
-
-    const deleteTask = async(id) => {
-
-      console.log("delete task called");
-
-      try {
-        await axios.delete(`/api/v1/delete/${id}`);
-        console.log(`Task deleted with the id ${id}`);
-        fetchData();
-      } catch (error) {
-        console.log(error);
-      }
-
-    }
-
-    const editTask = async(id,isDone) => {
-
-      const body = {
-        isDone : !isDone
-      };
-
-      try {
-        
-        await axios.put(`/api/v1/edit/${id}`,body);
-        // await axios.put(`/edit/${id}`,body);
-
-        console.log(`task updated successfully with the id ${id}`);
-
-        fetchData();
-
-      } catch (error) {
-        
-        console.log(error);
-
-      }
-
-    }
-
-    const addTask = async (e) => {
-      console.log(data);
-
-      e.preventDefault();
-
-      try {
-        await axios.post("api/v1/add", data);
-        console.log("API Call successful");
-        data.title = "";
-        addInputRef.current.focus();
-        fetchData();
-      } catch (error) {
-        console.log(error);
-      }
-    };
-
-    return (
-      <>
-        <div className="container-fluid position-relative bg-dark align-items-center d-flex flex-column">
-          <h1 className="text-custom-heading-color my-2 fw-bold">My Todos</h1>
-          {/* <div onClick={addTask} className="add-todo d-flex my-5">
-            <input
-              type="text"
-              htmlFor="title"
-              name="title"
-              placeholder="Add a new task..."
-              value={data.title}
-              onChange={(e) => setData({ ...data, title: e.target.value })}
-              className="bg-custom-secondary-color addtask-input rounded-3 p-2 text-custom-secondary-color"
-            />
-            <button className="" type="button" onClick={addTask}>
-              Add
-            </button>
-          </div> */}
-          <form className="add-todo d-flex my-5" onSubmit={addTask}>
-          <input
-            ref={addInputRef}
-              type="text"
-              name="title"
-              placeholder="Add a new task..."
-              value={data.title}
-              onChange={(e) => setData({ ...data, title: e.target.value })}
-              className="bg-custom-secondary-color addtask-input rounded-3 p-2 text-custom-secondary-color"
-              autoFocus
-            />
-            <button className="" type="submit" >
-              Add
-            </button>
-          </form>
-          <div className="list-todos">
-            {/* <li className="text-custom-primary-color">
-            <div className="todo-task">
-              <span><input type="checkbox" /></span>
-              task title
-              <span>icon 2</span>
-              <span>icon 3</span>
-            </div>
-          </li> */}
-            { todos.map((todo) => {
-              return(
-              <li className="text-custom-primary-color my-3" key={todo._id}>
-              <div className="todo-task d-flex bg-custom-secondary-color py-3 h-25">
-                {/* <span style={ {width:"5%"} } className="mx-2"><input type="checkbox" /></span> */}
-                <span style={{ width: "5%" }} className="checkSpan">
-                  <input onClick={() => editTask(todo._id,todo.isDone)} type="checkbox" checked={todo.isDone} id={`isTodoCompleted-${todo._id}`} className="isTodoCompleted" hidden/>
-                  <label
-                    htmlFor={`isTodoCompleted-${todo._id}`}
-                    className="isTodoCompletedLabel"
-                  >
-                    <span className="line line1"></span>
-                    <span className="line line2"></span>
-                  </label>
-                </span>
-                <span style={{ width: "85%" }} className="titleDisplaySpan">{todo.title}</span>
-                {/* <span className="" style={{ width: "5%" }}> */}
-                <span  style={{ width: "5%" }}><FontAwesomeIcon className="editTask" icon={faPenToSquare}></FontAwesomeIcon></span>
-                {/* </span> */}
-                {/* <span className="" style={{ width: "5%" }}>ic</span> */}
-                <span onClick={() => deleteTask(todo._id)} style={{ width: "5%" }}><FontAwesomeIcon  className="deleteTask" icon={faTrash}></FontAwesomeIcon></span>
-                {/* <span className="" style={{ width: "5%" }}><FontAwesomeIcon icon="fa-regular fa-pen-to-square"></FontAwesomeIcon></span> */}
-              </div>
-            </li>)
-            })
-            }
-          </div>
-        </div>
-      </>
-    );
   };
 
-  export default App;
+  const editTodoTitle = async (id) => {
+    const body = {
+      title: editData.title,
+    };
+
+    try {
+      await axios.put(`/api/v1/edit/${id}`, body);
+      fetchData();
+      setEditing(null);
+      setEditData({
+        title: "",
+      });
+      console.log("Edit Todo Title called after clicking check butotn");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const deleteTask = async (id) => {
+    console.log("delete task called");
+
+    try {
+      await axios.delete(`/api/v1/delete/${id}`);
+      console.log(`Task deleted with the id ${id}`);
+      fetchData();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const editTask = async (id, isDone) => {
+    const body = {
+      isDone: !isDone,
+    };
+
+    try {
+      await axios.put(`/api/v1/edit/${id}`, body);
+
+      console.log(`task updated successfully with the id ${id}`);
+
+      fetchData();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const addTask = async (e) => {
+    console.log(data);
+
+    e.preventDefault();
+
+    try {
+      await axios.post("api/v1/add", data);
+      console.log("API Call successful");
+      data.title = "";
+      addInputRef.current.focus();
+      fetchData();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // useEffect
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      <div className="container-fluid position-relative bg-dark align-items-center d-flex flex-column">
+        <h1 className="text-custom-heading-color my-2 fw-bold">My Todos</h1>
+        <form className="add-todo d-flex my-5" onSubmit={addTask}>
+          <input
+            ref={addInputRef}
+            type="text"
+            name="title"
+            placeholder="Add a new task..."
+            value={data.title}
+            onChange={(e) => setData({ ...data, title: e.target.value })}
+            className="bg-custom-secondary-color addtask-input rounded-3 p-2 text-custom-secondary-color"
+            autoFocus
+          />
+          <button className="" type="submit">
+            Add
+          </button>
+        </form>
+        <div className="list-todos">
+          {todos.map((todo) => {
+            return (
+              <li className="text-custom-primary-color my-3" key={todo._id}>
+                <div className="todo-task d-flex bg-custom-secondary-color py-3 h-25">
+                  <span style={{ width: "5%" }} className="checkSpan">
+                    <input
+                      onClick={() => editTask(todo._id, todo.isDone)}
+                      type="checkbox"
+                      checked={todo.isDone}
+                      id={`isTodoCompleted-${todo._id}`}
+                      className="isTodoCompleted"
+                      hidden
+                    />
+                    <label
+                      htmlFor={`isTodoCompleted-${todo._id}`}
+                      className="isTodoCompletedLabel"
+                    >
+                      <span className="line line1"></span>
+                      <span className="line line2"></span>
+                    </label>
+                  </span>
+                  {isEditing === todo._id ? (
+                    <input
+                      // ref={editInputRef}
+                      autoFocus
+                      style={{ width: "85%" }}
+                      className="titleDisplayInput"
+                      type="text"
+                      value={editData.title}
+                      onChange={(e) => {
+                        console.log("edit data input field called");
+                        setEditData({ ...editData, title: e.target.value });
+                      }}
+                    ></input>
+                  ) : (
+                    <span style={{ width: "85%" }} className="titleDisplaySpan">
+                      {todo.title}
+                    </span>
+                  )}
+                  {isEditing === todo._id ? (
+                    <span style={{ width: "5%" }}>
+                      <FontAwesomeIcon
+                        onClick={(e) => {
+                          // setEditing(null);
+                          // setEditData({ ...editData, title: e.target.value });
+                          // setEditData(editData);
+                          editTodoTitle(todo._id);
+                        }}
+                        className="editTask"
+                        icon={faCheck}
+                      ></FontAwesomeIcon>
+                    </span>
+                  ) : (
+                    <span style={{ width: "5%" }}>
+                      <FontAwesomeIcon
+                        className="editTask"
+                        icon={faPenToSquare}
+                        onClick={() => {
+                          setEditing(todo._id);
+                          console.log("edit button clicked");
+                          setEditData({ ...editData, title: todo.title });
+                        }}
+                      ></FontAwesomeIcon>
+                    </span>
+                  )}
+                  <span
+                    onClick={() => deleteTask(todo._id)}
+                    style={{ width: "5%" }}
+                  >
+                    <FontAwesomeIcon
+                      className="deleteTask"
+                      icon={faTrash}
+                    ></FontAwesomeIcon>
+                  </span>
+                </div>
+              </li>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default App;
