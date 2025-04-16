f88150196827f956e0883b3d50ed89de59fa5b85
kirtan-bhavsar <kirtanbhavsar.2491@gmail.com>
2025-04-16 12:44:48 +0530

    added_TodoModel_UserDependency_Dt:16042025

---
+++ server/migrateExistingTodos.js
@@ -0,0 +1,27 @@
+import User from "./Models/User.js";
+import Todo from "./Models/Todo.js";
+import config from "dotenv/config";
+import mongoose from "mongoose";
+
+const migrateExistingTodos = async () => {
+  await mongoose.connect(process.env.MONGO_URI);
+
+  console.log("Database connected successfully");
+
+  const todos = await Todo.find({ user: { $exists: false } });
+
+  console.log(todos);
+
+  const emailId = "kirtanbhavsar.2491@gmail.com";
+
+  const user = await User.find({ email: emailId });
+
+  console.log(user[0].id);
+
+  const testTodo = await Todo.findOne({ _id: "67f2d234373edd4f906e8916" });
+
+  // console.log(JSON.parse(testTodo));
+  console.log(testTodo._id);
+};
+
+migrateExistingTodos();
