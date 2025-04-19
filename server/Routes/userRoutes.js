041cbd07ef73856182641f07f543206932369b0d
kirtan-bhavsar <kirtanbhavsar.2491@gmail.com>
2025-04-20 01:43:55 +0530

    added_userController_implementedLogoutUserFunctionality_Dt:20042025

--- server/Routes/userRoutes.js
+++ server/Routes/userRoutes.js
@@ -1,10 +1,17 @@
 import express from "express";
-import { loginUser, registerUser } from "../Controllers/userController.js";
+import {
+  loginUser,
+  registerUser,
+  logoutUser,
+} from "../Controllers/userController.js";
+import auth from "../middleware/auth.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/register", registerUser);
 
 userRouter.post("/login", loginUser);
+
+userRouter.post("/logout", auth, logoutUser);
 
 export default userRouter;
