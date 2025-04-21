d60e640ab6e10938ee44e2c2e2222fb7277625e5
kirtan-bhavsar <kirtanbhavsar.2491@gmail.com>
2025-04-22 00:30:24 +0530

    added_Login_implementedAuthorizeFunctionality_Dt:22042025

--- client/Pages/Login.jsx
+++ client/Pages/Login.jsx
@@ -1,9 +1,41 @@
-import react, { useEffect } from "react";
+import react, { useEffect, useState } from "react";
 import LoginForm from "../Components/LoginForm.jsx";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Login = () => {
 
+  const navigate = useNavigate();
+
+  const[isAuthenticated,setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+
+   const checkLoginStatus = async() => {
+
+    console.log("checkLoginStatus called");
+
+      try {
+        
+      const response =   await axios.get("/api/v1/user/auth");
+
+      if(response.status === 200){
+        console.log(response);
+        setIsAuthenticated(true);
+        navigate('/home');
+      } else {
+        setIsAuthenticated(fale);
+      }
+
+      } catch (error) {
+        console.log(error);
+      }
+
+    }
+
+    checkLoginStatus();
+
+  },[]);
 
   return (
     <>
