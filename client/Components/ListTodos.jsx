c4554cad40db987a3273fdacd0862106d92efc3b
kirtan-bhavsar <kirtanbhavsar.2491@gmail.com>
2025-04-30 23:41:03 +0530

    implemented_ListTodos.jsx&index.css_addedCheckTodoAnimation_Dt:30042025

--- client/Components/ListTodos.jsx
+++ client/Components/ListTodos.jsx
@@ -9,7 +9,7 @@ import { faCheck } from "@fortawesome/free-solid-svg-icons";
 const ListTodos = ({todos,editTask,isEditing,editData,setEditData,editTodoTitle,setEditing ,deleteTask}) => {
   return (
     <><div className="list-todos">
-              { todos.length === 0 ? (<p className='text-custom-primary-color py-5 text-center'>No incomplete Todos Left !! Add New Ones</p>) : (
+              { todos.length === 0 ? (<p className='text-custom-primary-color py-5 text-center'>No Todos Left !! Add New Ones</p>) : (
               todos.map((todo) => {
                 return (
                   <li className='text-custom-primary-color singleTodoContainer my-3' key={todo._id}>
